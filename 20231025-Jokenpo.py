39dbcb99e18e6097efecfdf65adee997a4c8b932
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2023-11-03 15:42:05 -0300

    Update 20231025-Jokenpo.py
    Updated version of my jokenpo game, using new concepts I learned.

--- 20231025-Jokenpo.py
+++ 20231025-Jokenpo.py
@@ -1,29 +1,134 @@
+from time import sleep
 import random
-pedra = "pedra"
-papel = "papel"
-tesoura = "tesoura"
-opcoes = [pedra, papel, tesoura]
+possibilidades = ['PEDRA', 'PAPEL', 'TESOURA']
 
-while True:
-    escolha_user = input("Pedra, papel ou tesoura? Digite sua escolha: \n")
-    escolha_user = escolha_user.lower()
+print('\nAproxime-se desafiante...')
+sleep(0.5)
+print('SEJA BEM VINDO AO DESAFIO JÓ KEN PÔ!')
+sleep(1)
+vitorias = 0
+derrotas = 0
 
-    if escolha_user != "pedra" and escolha_user != "papel" and escolha_user != "tesoura":
-        print("Escolha uma opção válida!")
+def pedrapapeltesoura():
+    print("\nVocê já sabe as regras...")
+    sleep(0.5)
+    escolha_user = str(input('Portanto, qual você escolhe: pedra, papel ou tesoura? ').strip()).upper()
+    if escolha_user in possibilidades:
+        escolha_cpu = random.choice(possibilidades)
+        print("Então vamos lá!\n")
+        sleep(0.5)
+        print('JÓ')
+        sleep(0.5)
+        print('KEN')
+        sleep(0.5)
+        print('PÔ!\n')
+        sleep(0.5)
+        if escolha_user == 'PEDRA' and escolha_cpu == 'TESOURA' or escolha_user == 'PAPEL' and escolha_cpu == 'PEDRA' or escolha_user == 'TESOURA' and escolha_cpu == 'PAPEL':
+            if escolha_user == "PEDRA": escolha_user = escolha_user.replace("PEDRA", "pedra 👊")
+            if escolha_user == "PAPEL": escolha_user = escolha_user.replace("PAPEL", "papel 🖐️")
+            if escolha_user == "TESOURA": escolha_user = escolha_user.replace("TESOURA", "tesoura ✌️")
+            if escolha_cpu == "PEDRA": escolha_cpu = escolha_cpu.replace("PEDRA", "pedra 👊")
+            if escolha_cpu == "PAPEL": escolha_cpu = escolha_cpu.replace("PAPEL", "papel 🖐️")
+            if escolha_cpu == "TESOURA": escolha_cpu = escolha_cpu.replace("TESOURA", "tesoura ✌️")
+            print(f'Parece que você escolheu: {escolha_user}.')
+            sleep(1)
+            print(f'Eu escolhi: {escolha_cpu}.')
+            sleep(1)
+            print('\n...')
+            sleep(1)
+            print('Você venceu...')
+            sleep(1)
+            global vitorias
+            vitorias += 1
+            revanche()
+        elif escolha_user == 'TESOURA' and escolha_cpu == 'PEDRA' or escolha_user == 'PEDRA' and escolha_cpu == 'PAPEL' or escolha_user == 'PAPEL' and escolha_cpu == 'TESOURA':
+            if escolha_user == "PEDRA": escolha_user = escolha_user.replace("PEDRA", "pedra 👊")
+            if escolha_user == "PAPEL": escolha_user = escolha_user.replace("PAPEL", "papel 🖐️")
+            if escolha_user == "TESOURA": escolha_user = escolha_user.replace("TESOURA", "tesoura ✌️")
+            if escolha_cpu == "PEDRA": escolha_cpu = escolha_cpu.replace("PEDRA", "pedra 👊")
+            if escolha_cpu == "PAPEL": escolha_cpu = escolha_cpu.replace("PAPEL", "papel 🖐️")
+            if escolha_cpu == "TESOURA": escolha_cpu = escolha_cpu.replace("TESOURA", "tesoura ✌️")
+            print(f'Parece que você escolheu: {escolha_user}.')
+            sleep(1)
+            print(f'Eu escolhi: {escolha_cpu}.')
+            sleep(1)
+            print('\n...')
+            sleep(1)
+            print('HA, HÁ... Eu ganhei! ̶ ̶o̶t̶á̶r̶i̶o̶ ')
+            sleep(1)
+            global derrotas
+            derrotas += 1
+            revanche()
+        elif escolha_cpu == escolha_user:
+            if escolha_user == "PEDRA": escolha_user = escolha_user.replace("PEDRA", "pedra 👊")
+            if escolha_user == "PAPEL": escolha_user = escolha_user.replace("PAPEL", "papel 🖐️")
+            if escolha_user == "TESOURA": escolha_user = escolha_user.replace("TESOURA", "tesoura ✌️")
+            if escolha_cpu == "PEDRA": escolha_cpu = escolha_cpu.replace("PEDRA", "pedra 👊")
+            if escolha_cpu == "PAPEL": escolha_cpu = escolha_cpu.replace("PAPEL", "papel 🖐️")
+            if escolha_cpu == "TESOURA": escolha_cpu = escolha_cpu.replace("TESOURA", "tesoura ✌️")
+            print(f'Parece que você escolheu: {escolha_user}.')
+            sleep(1)
+            print(f'Eu escolhi: {escolha_cpu}.')
+            sleep(1)
+            print('\n...')
+            sleep(1)
+            print('Deu empate. Vamos tentar de novo!')
+            sleep(1)
+            pedrapapeltesoura()
+        else:
+            print("Essa é uma opção inválida. Tente novamente.")
+            pedrapapeltesoura()
     else:
-        break    
+        print("Essa é uma opção inválida. Tente novamente.")
+        pedrapapeltesoura()
 
-escolha_comput = random.choice(opcoes)
-print(f"Você escolheu {escolha_user} e eu escolhi {escolha_comput}.")
+def revanche():
+    revanche = input('Quer jogar de novo? [sim/não]: ').lower()
+    revanche = revanche.strip()
+    if revanche == 'sim':
+        print('\nEntão bora!')
+        pedrapapeltesoura()
+    elif revanche == 'não' or revanche == 'nao':
+        print('\nOh, que pena...\n')
+        sleep(1)
+        resultados()
+    else:
+        print("Essa é uma opção inválida. Tente novamente.")
+        pedrapapeltesoura()
 
-condvitoria = escolha_user == pedra and escolha_comput == tesoura or escolha_user == papel and escolha_comput == pedra or escolha_user == tesoura and escolha_comput == papel
-condempate = escolha_user == escolha_comput
+def resultados():
+    print("Bem... isso foi divertido, mas vamos ao que interessa...")
+    sleep(1.5)
+    print("Seus resultados são...\n")
+    sleep(1)
+    print(f'Vitórias: {vitorias}.')
+    sleep(1)
+    print(f'Derrotas: {derrotas}\n')
+    sleep(1)
+    if vitorias == derrotas:
+        print('Parece que dessa vez empatamos.')
+        sleep(0.5)
+        print('Mas garanto que te venço na próxima.')
+        sleep(1)
+        print('Até mais!')
+    elif vitorias > derrotas:
+        print("...")
+        sleep(1.5)
+        print("Ainda não sei como...", end=' ')
+        sleep(1)
+        print('mas tenho certeza que você trapaceou.')
+        sleep(1)
+        print("Hmpf... parabéns, você ganhou.")
+        sleep(0.5)
+        print('Agora suma da minha frente.')
+    else:
+        print("Como era de se esperar: eu venci!")
+        sleep(1)
+        print('Admita...', end=' ')
+        sleep(1)
+        print('você nunca teve chance contra minha incrível habilidade!')
+        sleep(1)
+        print('Sempre que quiser levar uma surra no JÓ KEN PÔ, estarei aqui.')
+        print('Adeus.')
 
-if not condvitoria and not condempate:
-    print("Que pena! Você perdeu.")
-
-if condvitoria:
-    print("Parabéns! Você ganhou!")
-
-if condempate:
-    print("Parece que deu empate...")
+pedrapapeltesoura()
